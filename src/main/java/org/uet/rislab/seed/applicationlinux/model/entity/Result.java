f0c81ea426710e2e92e8c6cfeb165059c831234a
hungha1512 <hungha15123@gmail.com>
2025-01-07 09:41:21 +0700

    feat: update Result entity

--- src/main/java/org/uet/rislab/seed/applicationlinux/model/entity/Result.java
+++ src/main/java/org/uet/rislab/seed/applicationlinux/model/entity/Result.java
@@ -1,18 +1,27 @@
 package org.uet.rislab.seed.applicationlinux.model.entity;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-
 public class Result {
-    private String resultId;
-    private Date analysisDate;
-    private Map<String, Double> measurements;
-    private Seed seed;
-    private Project project;
+    private final int id;
+    private final double height;
+    private final double width;
 
-    public Result() {
-        this.measurements = new HashMap<>();
+    public Result(int id, double height, double width) {
+        this.id = id;
+        this.height = height;
+        this.width = width;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
 }
+
+
